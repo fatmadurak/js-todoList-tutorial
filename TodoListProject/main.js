b20c1ede90e2583f9c844d7407b7dc07b73e4f3a
fatmadurak <fatmabadillidurak@gmail.com>
2023-10-06 14:40:37 +0300

    add and remove todo codes

--- TodoListProject/main.js
+++ TodoListProject/main.js
@@ -1,27 +1,43 @@
-const todosList = document.querySelector(".todos-list"); // Tek bir öğeyi seçmek için querySelector kullanıyoruz.
-const input1 = document.querySelector(".input1"); // Tek bir öğeyi seçmek için querySelector kullanıyoruz.
-const btn = document.querySelector(".btn"); // Tek bir öğeyi seçmek için querySelector kullanıyoruz.
-const closeIcon =document.getElementsByTagName(i);
+
+//choose elements
+const todosList = document.querySelector(".todos-list");
+const input1 = document.querySelector(".input1");
+const btn = document.querySelector(".btn");
+
+
+
 
 
 function handleInputChange(e) {
   const value = e.target.value;
 }
 
+//button change
+
 input1.addEventListener("change", handleInputChange);
 
-btn.addEventListener("click", function () {
-  const newItem = document.createElement("li");
-  newItem.classList.add("todo-item");
-
-  const newIcon = document.createElement("i");
-  newIcon.classList.add("bi", "bi-x-lg");
-  newItem.textContent=input1.value;
-
-  newItem.appendChild(newIcon);
-  todosList.appendChild(newItem);
-});
 
 
+function addandRemoveTodo() {
+    const newItem = document.createElement("li");
+    newItem.classList.add("todo-item");
+  
+    const newIcon = document.createElement("i");
+    newIcon.classList.add("bi", "bi-x-lg");
+    newItem.textContent = input1.value;
+  
+    newIcon.addEventListener("click", function () {
+      todosList.removeChild(newItem); // "li" öğesini listeden kaldırır.
+    });
+  
+    newItem.appendChild(newIcon);
+    todosList.appendChild(newItem);
+    input1.value=""
+  }
+
+
+//button click add todo
+
+btn.addEventListener("click",addandRemoveTodo );
 
 
